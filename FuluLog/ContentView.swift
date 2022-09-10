061795a6bd1d93df4f1406bf7067d695fdc0db75
ame <shachi10969@gmail.com>
2022-09-10 21:40:46 +0900

    Initial Commit

---
+++ FuluLog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FuluLog
+//
+//  Created by t&a on 2022/09/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FuluLog/FuluLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuluLogApp.swift
+//  FuluLog
+//
+//  Created by t&a on 2022/09/10.
+//
+
+import SwiftUI
+
+@main
+struct FuluLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
